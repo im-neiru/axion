902a0b1897f2ebca63fafb54b9cef42bfc9a3320
neilcantorne <cantorne.neil@gmail.com>
2023-08-30 05:04:41 +0800

    chore(docs): add description of functions

--- core/src/math/sse2/fvector2.rs
+++ core/src/math/sse2/fvector2.rs
@@ -10,6 +10,12 @@ use std::mem::MaybeUninit;
 // Import from crate
 use crate::math::{Vector, Vector2};
 
+/// `FVector2` is a structure that represents a 2D vector with `f32` components.
+/// It encapsulates two floating-point values and is used for various purposes in graphical applications
+/// including points, vectors, and texture coordinates.
+///
+/// This structure is implemented with SIMD (Single Instruction, Multiple Data) which is a type of parallel computing
+/// involving vectors that allows for multiple data points to be processed at once, resulting in performance improvements.
 #[derive(Clone, Copy)]
 #[repr(transparent)]
 pub struct FVector2(pub(crate) __m128);
@@ -21,12 +27,20 @@ impl Default for FVector2 {
     }
 }
 
+/// This is a Vector2 implementation for f32 type.
 impl Vector2<f32> for FVector2 {
+    /// Constructs a new `FVector2`.
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - A float that holds the x component of the vector.
+    /// * `y` - A float that holds the y component of the vector.
     #[inline]
     fn new(x: f32, y: f32) -> Self {
         Self(unsafe { _mm_set_ps(0.0, 0.0, y, x) })
     }
 
+    /// Returns the x component of the vector.
     #[inline]
     #[allow(clippy::uninit_assumed_init)]
     #[allow(invalid_value)]
@@ -39,6 +53,7 @@ impl Vector2<f32> for FVector2 {
         }
     }
 
+    /// Returns the y component of the vector.
     #[inline]
     #[allow(clippy::uninit_assumed_init)]
     #[allow(invalid_value)]
@@ -53,6 +68,7 @@ impl Vector2<f32> for FVector2 {
         }
     }
 
+    /// Returns the x and y components of the vector as a tuple.
     #[inline]
     #[allow(clippy::uninit_assumed_init)]
     #[allow(invalid_value)]
@@ -69,18 +85,21 @@ impl Vector2<f32> for FVector2 {
         }
     }
 
+    /// Returns the y and x components of the vector as a tuple.
     #[inline]
     fn yx(self) -> (f32, f32) {
         let xy = self.xy();
         (xy.1, xy.0)
     }
 
+    /// Returns the x component of the vector twice as a tuple.
     #[inline]
     fn xx(self) -> (f32, f32) {
         let x = self.x();
         (x, x)
     }
 
+    /// Returns the y component of the vector twice as a tuple.
     #[inline]
     fn yy(self) -> (f32, f32) {
         let y = self.y();
@@ -89,6 +108,11 @@ impl Vector2<f32> for FVector2 {
 }
 
 impl Vector<f32> for FVector2 {
+    /// Returns the dot product of the vector and another vector.
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Another vector to calculate the dot product with.
     #[inline]
     #[allow(clippy::uninit_assumed_init)]
     #[allow(invalid_value)]
@@ -104,16 +128,40 @@ impl Vector<f32> for FVector2 {
         }
     }
 
+    /// Returns the length (magnitude) of the vector.
+    ///
+    /// The length of a vector is a non-negative number that describes the extent of the vector in space. It is always
+    /// positive unless the vector has all components equal to zero, in which case the length is zero.
+    ///
+    /// The length is calculated by first squaring the components of the vector (`length_sq` function), and then taking
+    /// the square root of the sum.
+    ///
+    /// This function may be used in graphics programming to determine the distance of a point represented by the vector
+    /// from the origin of the vector space. As such, it can be used to calculate distances between points.
     #[inline]
     fn length(self) -> f32 {
         self.length_sq().sqrt()
     }
 
+    /// Returns the squared length of the vector.
+    ///
+    /// This function is often used in graphics programming when comparing lengths, as it avoids the computationally
+    /// expensive square root operation. In comparison operations, the exact length is often not necessary,
+    /// so the square length can be used instead.
+    ///
+    /// The squared length is calculated by performing the dot product of the vector with itself (`dot` function).
     #[inline]
     fn length_sq(self) -> f32 {
         self.dot(self)
     }
 
+    /// Returns the inverse of the length (reciprocal of the magnitude) of the vector.
+    ///
+    /// The inverse length may be used in graphics programming to normalize a vector. Normalization refers to the process
+    /// of scaling the vector to make its length equal to one, while preserving its direction. By multiplying a vector
+    /// by its inverse length, we can quickly obtain a unit vector.
+    ///
+    /// The inverse length is calculated by taking the reciprocal of the length of the vector (`length` function).
     #[inline]
     fn length_inv(self) -> f32 {
         self.length().recip()
