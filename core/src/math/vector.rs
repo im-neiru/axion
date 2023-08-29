46e9d22d61f216755240c068c7f7e35e6ca22739
neilcantorne <cantorne.neil@gmail.com>
2023-08-29 14:56:20 +0800

    feat: add `Vector` trait

---
+++ core/src/math/vector.rs
@@ -0,0 +1,21 @@
+pub trait Vector<T>
+where
+    Self: Copy + Clone + Default,
+    T: Copy + Clone + Default,
+{
+    fn dot(self, other: Self) -> T;
+    fn length(self) -> T;
+    fn length_sq(self) -> T;
+    fn length_inv(self) -> T;
+}
+pub trait Vector2<T>: Vector<T>
+where
+    Self: Vector<T>,
+    T: Copy + Clone + Default,
+{
+    fn new(x: T, y: T) -> Self;
+    fn x(self) -> T;
+    fn y(self) -> T;
+    fn xy(self) -> (T, T);
+    fn yx(self) -> (T, T);
+}
