e3223c75988d1d2bfe6b18ee352c611f2a85e5ee
im-neiru <cantorne.neil@gmail.com>
2023-09-10 07:02:37 +0800

    feat: impl axis constants for `FVector2`

--- core/src/math/sse/fvector2.rs
+++ core/src/math/sse/fvector2.rs
@@ -86,6 +86,26 @@ impl FVector2 {
     /// initialized to the smallest positive value representable by `f32`.
     pub const EPSILON: Self = Self::splat(f32::EPSILON);
 
+    /// A constant `FVector2` representing the positive X-axis.
+    ///
+    /// This constant vector has a value of `(1.0, 0.0)`, representing the positive X-axis in 2D space.
+    pub const AXIS_X: Self = Self { x: 1.0, y: 0.0 };
+
+    /// A constant `FVector2` representing the positive Y-axis.
+    ///
+    /// This constant vector has a value of `(0.0, 1.0)`, representing the positive Y-axis in 2D space.
+    pub const AXIS_Y: Self = Self { x: 0.0, y: 1.0 };
+
+    /// A constant `FVector2` representing the negative X-axis.
+    ///
+    /// This constant vector has a value of `(-1.0, 0.0)`, representing the negative X-axis in 2D space.
+    pub const NEG_AXIS_X: Self = Self { x: -1.0, y: 0.0 };
+
+    /// A constant `FVector2` representing the negative Y-axis.
+    ///
+    /// This constant vector has a value of `(0.0, -1.0)`, representing the negative Y-axis in 2D space.
+    pub const NEG_AXIS_Y: Self = Self { x: 0.0, y: -1.0 };
+
     /// Constructs a new `FVector2`.
     ///
     /// # Arguments
