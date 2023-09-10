41026fca5c1af4ff963eae9d581101fcbf90a0e8
im-neiru <cantorne.neil@gmail.com>
2023-09-11 06:40:16 +0800

    feat: impl `FVector4`

--- core/src/math/mod.rs
+++ core/src/math/mod.rs
@@ -8,4 +8,4 @@ mod sse;
 // Public structs
 #[cfg(feature = "simd")]
 #[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-pub use sse::{FVector2, FVector3};
+pub use sse::{FVector2, FVector3, FVector4};
--- core/src/math/sse/mod.rs
+++ core/src/math/sse/mod.rs
@@ -3,10 +3,12 @@
 // Sub modules
 mod fvector2;
 mod fvector3;
+mod fvector4;
 
 // Public structs
 pub use fvector2::FVector2;
 pub use fvector3::FVector3;
+pub use fvector4::FVector4;
 
 /// Create an SSE shuffle mask from component indices.
 ///
