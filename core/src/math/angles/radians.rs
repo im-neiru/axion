28f2144312b3a8b3fead53b0d30ff670b99446bd
Neiru <cantorne.neil@gmail.com>
2023-09-17 01:55:24 +0800

    feat: impl trigonometric functions for `Radians`

--- core/src/math/angles/radians.rs
+++ core/src/math/angles/radians.rs
@@ -124,6 +124,204 @@ impl Radians {
     }
 }
 
+impl Radians {
+    /// Returns the cosine of the angle in radians.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let cos = pi_half.cos();
+    /// ```
+    #[inline]
+    pub fn cos(self) -> f32 {
+        (self.0).cos()
+    }
+
+    /// Returns the sine of the angle in radians.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let sin = pi_half.sin();
+    /// ```
+    #[inline]
+    pub fn sin(self) -> f32 {
+        (self.0).sin()
+    }
+
+    /// Returns the tangent of the angle in radians.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let tan = pi_half.tan();
+    /// ```
+    #[inline]
+    pub fn tan(self) -> f32 {
+        (self.0).tan()
+    }
+
+    /// Returns the secant of the angle in radians.
+    ///
+    /// The secant is the reciprocal of the cosine.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let sec = pi_half.sec();
+    /// ```
+    #[inline]
+    pub fn sec(self) -> f32 {
+        (self.0).cos().recip()
+    }
+
+    /// Returns the cosecant of the angle in radians.
+    ///
+    /// The cosecant is the reciprocal of the sine.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let csc = pi_half.csc();
+    /// ```
+    #[inline]
+    pub fn csc(self) -> f32 {
+        (self.0).sin().recip()
+    }
+
+    /// Returns the cotangent of the angle in radians.
+    ///
+    /// The cotangent is the reciprocal of the tangent.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi_half = Radians::PI_HALF;
+    /// let cot = pi_half.cot();
+    /// ```
+    #[inline]
+    pub fn cot(self) -> f32 {
+        (self.0).tan().recip()
+    }
+
+    /// Returns a unit vector in the direction of the angle in radians.
+    ///
+    /// The `normal` method computes the cosine and sine of the angle in radians
+    /// and returns a `axion::math::FVector2` representing the unit vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let pi = Radians::PI;
+    /// let normal_vector = pi.normal();
+    /// ```
+    #[inline]
+    pub fn normal(self) -> crate::math::FVector2 {
+        let (cos, sin) = (self.0.cos(), self.0.sin());
+        crate::math::FVector2 { x: cos, y: sin }
+    }
+
+    /// Computes the arccosine of the `scalar` value in radians.
+    ///
+    /// The result is in the range \[*0* rad, *π* rad\], or NaN if the `scalar` is
+    /// outside the range \[`-1.0`, `1.0`\].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let scalar = 0.5;
+    /// let angle = Radians::acos(scalar);
+    /// ```
+    #[inline]
+    pub fn acos(scalar: f32) -> Self {
+        Self(scalar.acos())
+    }
+
+    /// Computes the arcsine of the `scalar` value in radians.
+    ///
+    /// The result is in the range \[*-½π* rad, *½π* rad\], or `NaN` if the `scalar` is
+    /// outside the range \[`-1.0`, `1.0`\].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let scalar = 0.5;
+    /// let angle = Radians::asin(scalar);
+    /// ```
+    #[inline]
+    pub fn asin(scalar: f32) -> Self {
+        Self(scalar.asin())
+    }
+
+    /// Computes the arc tangent of the `scalar` value in radians.
+    ///
+    /// The result is in the range \[*-½π* rad, *½π* rad\].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let scalar = 0.5;
+    /// let angle = Radians::atan(scalar);
+    /// ```
+
+    #[inline]
+    pub fn atan(scalar: f32) -> Self {
+        Self(scalar.atan())
+    }
+
+    /// Computes the four-quadrant arctangent of the `y` and `x` coordinates
+    /// in radians, which corresponds to the vector direction.
+    ///
+    /// The result has the following range:
+    ///
+    ///   - When `x` = `0.0` and `y` = `0.0`: *0* rad
+    ///   - When `x` >= `0.0`: arctan(`y`/`x`) -> \[*-½π* rad, *½π* rad\]
+    ///   - When `y` >= `0.0`: arctan(`y`/`x`) + π -> (*½π* rad, *π* rad]
+    ///   - When `y` < `0.0`: arctan(`y`/`x`) - π -> (-πrad, *-½π* rad)
+    ///
+    /// # Parameters
+    ///
+    /// * `y`: The `y` coordinate of the vector.
+    /// * `x`: The `x` coordinate of the vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use axion::math::Radians;
+    ///
+    /// let angle = Radians::atan2(1.0, 1.0);
+    /// ```
+    #[inline]
+    pub fn atan2(y: f32, x: f32) -> Self {
+        Self(f32::atan2(y, x))
+    }
+}
+
 impl fmt::Debug for Radians {
     /// Formats the `Radians` value for debugging purposes.
     ///
