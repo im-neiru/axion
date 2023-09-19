0535bea2b4c276569d801453b344edabbf8b517c
im-neiru <cantorne.neil@gmail.com>
2023-09-20 03:23:03 +0800

    feat: impl `ops:Mul` for `Quaternion`

--- core/src/math/ordinary/quaternion.rs
+++ core/src/math/ordinary/quaternion.rs
@@ -57,3 +57,50 @@ impl ops::Sub for Quaternion {
         }
     }
 }
+
+impl ops::Mul for Quaternion {
+    type Output = Self;
+
+    /// Multiplies this quaternion by another quaternion using the Hamilton product.
+    ///
+    /// # Parameters
+    ///
+    /// * `rhs`: A reference to the quaternion to multiply with.
+    ///
+    /// # Returns
+    ///
+    /// A new quaternion that is the result of the Hamilton product.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use axion::math::Quaternion;
+    ///
+    /// let quat1 = Quaternion::new(1.0, 2.0, 3.0, 4.0);
+    /// let quat2 = Quaternion::new(5.0, 6.0, 7.0, 8.0);
+    /// let result = quat1 * quat2;
+    /// assert_eq!(result, Quaternion::new(-60.0, 12.0, 30.0, 24.0));
+    /// ```
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            w: self.w.mul_add(
+                rhs.w,
+                self.x
+                    .mul_add(-rhs.x, self.y.mul_add(-rhs.y, -self.z * rhs.z)),
+            ),
+            x: self.w.mul_add(
+                rhs.x,
+                self.x
+                    .mul_add(rhs.w, self.y.mul_add(rhs.z, -self.z * rhs.y)),
+            ),
+            y: self.w * rhs.y - self.x * rhs.z
+                + self.y * rhs.w
+                + self.z * rhs.x,
+            z: self.w.mul_add(
+                rhs.z,
+                self.x
+                    .mul_add(rhs.y, self.y.mul_add(-rhs.x, self.z * rhs.w)),
+            ),
+        }
+    }
+}
